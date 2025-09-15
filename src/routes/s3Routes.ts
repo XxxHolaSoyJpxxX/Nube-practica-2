965f8dcfa3ccd91c7a9c835d65ddcf1971023c3f
Jp <117955216+XxxHolaSoyJpxxX@users.noreply.github.com>
2025-09-14 23:14:24 -0600

    Update s3Routes.ts

--- src/routes/s3Routes.ts
+++ src/routes/s3Routes.ts
@@ -17,7 +17,8 @@ const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    sessionToken: process.env.AWS_SESSION_TOKEN!
   }
 });
 const upload = multer({ storage: multer.memoryStorage() });
